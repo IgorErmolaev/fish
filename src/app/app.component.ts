efcab40986df2c4d488ace942ab4bbea6e766f3e
HZ210492EIA <igor.ermolaev@a-bank.com.ua>
2024-10-16 20:00:40 +0300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,5 +10,39 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  constructor(private http: HttpClient) {}
   title = 'fish';
+
+  ngOnInit(): void {
+    this.getUserIp();
+  }
+
+  getUserIp() {
+    this.http.get<{ip: string}>('https://api.ipify.org?format=json').subscribe(data => {
+      console.log('IP Address:', data.ip);
+      const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+    const language = navigator.language;
+
+    console.log('User Agent: ', userAgent);
+    console.log('Platform: ', platform);
+    console.log('Language: ', language);
+
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        console.log('Latitude: ', position.coords.latitude);
+        console.log('Longitude: ', position.coords.longitude);
+      });
+    } else {
+      console.log('Geolocation is not supported by this browser.');
+    }
+
+    });
+  }
+
+  goToPromo() {
+    window.location.href = '#promo';
+  }
+
 }
