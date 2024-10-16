3bd49c1f483795d5c18133ab8214ff024a4216d3
HZ210492EIA <igor.ermolaev@a-bank.com.ua>
2024-10-16 21:02:55 +0300

    google

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -15,11 +15,12 @@ export class AppComponent {
   title = 'fish';
 
   ngOnInit(): void {
-    this.getUserIp();
+    this.getUserIp("main");
   }
 
-  getUserIp() {
+  getUserIp(action:string) {
     this.http.get<{ip: string}>('https://api.ipify.org?format=json').subscribe(data => {
+      
       console.log('IP Address:', data.ip);
       const userAgent = navigator.userAgent;
     const platform = navigator.platform;
@@ -29,20 +30,43 @@ export class AppComponent {
     console.log('Platform: ', platform);
     console.log('Language: ', language);
 
+    const userInfo = {
+      ip: data.ip,
+      userAgent: userAgent,
+      platform: platform,
+      language: language,
+      latitude: 0,
+      longitude: 0,
+      action: action
+    };
+
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
+        userInfo.latitude = position.coords.latitude;
+        userInfo.longitude = position.coords.longitude;
         console.log('Latitude: ', position.coords.latitude);
         console.log('Longitude: ', position.coords.longitude);
+        this.sendDataToGoogleSheet(userInfo);
       });
     } else {
       console.log('Geolocation is not supported by this browser.');
+      this.sendDataToGoogleSheet(userInfo);
     }
+    
 
     });
   }
 
-  goToPromo() {
-    window.location.href = '#promo';
+  sendDataToGoogleSheet(userInfo: any) {
+    const headers = new HttpHeaders({
+    'Content-Type': 'application/json'
+  });
+    const googleSheetUrl = 'https://script.google.com/macros/s/AKfycbzBO71ZLU1VdRDsYhXD6Ol_hnZAoePJT1WtyZfSnR8M9ngXA03ZE-DXAIgQNwa93dPM/exec'; // URL для вебхука Google Sheets
+    this.http.post(googleSheetUrl, userInfo, { headers: headers }).subscribe(response => {
+      console.log('Data sent to Google Sheet:', response);
+    }, error => {
+      console.error('Error sending data to Google Sheet:', error);
+    });
   }
 
 }
